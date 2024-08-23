081af72fecd6dabbdb3beaa6f1080e7e41701bcc
yongen.loong <yongen.loong@aelf.io>
2024-08-23 12:21:49 +0800

    fix: timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5,
+  timeout: 5 * 1000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
